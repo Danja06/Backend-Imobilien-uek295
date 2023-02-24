f75c68881609a08728c16bbb391f1222d285f3d5
Daniil Koch <dko140656@stud.gibb.ch>
2023-02-24 09:46:57 +0100

    initial commit

---
+++ src/main/java/ch/noseryoung/imobilien/ImobilienApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.imobilien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobilienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImobilienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/noseryoung/imobilien/ImobilienApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.imobilien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobilienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
