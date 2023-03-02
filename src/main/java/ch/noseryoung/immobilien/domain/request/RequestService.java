96b4c46835ab954290fe7de81e372ef7c7311f45
Danja06 <dko140656@stud.gibb.ch>
2023-03-02 21:47:17 +0100

    new file commit

--- src/main/java/ch/noseryoung/immobilien/domain/request/RequestService.java
+++ src/main/java/ch/noseryoung/immobilien/domain/request/RequestService.java
@@ -6,5 +6,5 @@ public interface RequestService{
     Request create(Request request);
     Request acceptedStatus(Status ACCEPTED, UUID id);
     Request deniedStatus(Status DENIED, UUID id);
-    Request
+
 }
---
+++ src/main/java/ch/noseryoung/immobilien/domain/request/RequestServiceImpl.java
@@ -0,0 +1,29 @@
+package ch.noseryoung.immobilien.domain.request;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class RequestServiceImpl implements RequestService{
+    private RequestRepository requestRepository;
+
+    public RequestServiceImpl(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @Override
+    public Request create(Request request) {
+        return requestService.create(request);
+    }
+
+    @Override
+    public Request acceptedStatus(Status ACCEPTED, UUID id) {
+        return null;
+    }
+
+    @Override
+    public Request deniedStatus(Status DENIED, UUID id) {
+        return null;
+    }
+}
